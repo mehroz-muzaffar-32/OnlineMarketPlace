edd0c25b8bf1e4c4f987908cd168ac4aac05f61b
FaizanMuhammadSE <bsef18m023@pucit.edu.pk>
2021-05-25 19:46:19 +0500

    Update Startup.cs

--- OnlineMarketPlace/Startup.cs
+++ OnlineMarketPlace/Startup.cs
@@ -46,7 +46,7 @@ namespace OnlineMarketPlace
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=BuyerView}/{id?}");
             });
         }
     }
