a1e0f632abffffbb981b530548cdff899eca23ec
mehroz-muzaffar-SEF18 <79619614+mehroz-muzaffar-SEF18@users.noreply.github.com>
2021-05-31 04:34:00 +0500

    M004 Components Added

--- OnlineMarketPlace_Blazor/Models/Item.cs
+++ OnlineMarketPlace_Blazor/Models/Item.cs
@@ -8,9 +8,10 @@ namespace OnlineMarketPlace_Blazor.Models
     public  class Item
     {
         public string ItemName { get; set; }
-        public string Brand { get; set; }
+        public string Brand { get; set; } = "None";
         public string Image { get; set; }
         public decimal Price { get; set; }
         public decimal Discount { get; set; }
+        public string Category { get; set; } = "Others";
     }
 }
