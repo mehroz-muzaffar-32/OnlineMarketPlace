7156f67c99c2a1d872214f72f5f2ecb5152971c1
mehroz-muzaffar-SEF18 <79619614+mehroz-muzaffar-SEF18@users.noreply.github.com>
2021-06-03 04:10:37 +0500

    Project Successfully Shifted to Core Hosted One

---
+++ OnlineMarketPlace/Shared/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMarketPlace.Shared
+{
+    public  class Item
+    {
+        public string ItemName { get; set; }
+        public string Brand { get; set; } = "None";
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public string Category { get; set; } = "Other";
+        public int Quantity { get; set; } = 0;
+    }
+}
---
+++ OnlineMarketPlace/Shared/ItemsRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMarketPlace.Shared
+{
+    public class ItemsRepository
+    {
+        static public string View { get; set; } = "List";
+        static public List<Item> ItemsList = new List<Item>
+        {
+            new Item{ItemName="Shaper Shoes",Brand="Shaper",Image="/shoes.jpg",Price=1234,Discount=12},
+            new Item{ItemName="Nike Shoes",Image="/shoes2.jpg",Price=12000,Discount=30},
+            new Item{ItemName="Ladies Shoes",Image="/shoes3.jpg",Price=40000,Discount=20},
+            new Item{ItemName="Baby Shoes",Image="/shoes4.jpg",Price=50000,Discount=10},
+            new Item{ItemName="Red Berry",Image="/shirt.jpg",Price=40000,Discount=5},
+            new Item{ItemName="Brown Digger",Image="/watch.jpg",Price=900000,Discount=15},
+            new Item{ItemName="Office Love",Image="/watch2.jpg",Price=35000,Discount=25},
+            new Item{ItemName="Gold Mine",Image="/watch3.jpg",Price=400000,Discount=30},
+            new Item{ItemName="Royal",Image="/watch4.jpg",Price=5000000,Discount=35},
+            new Item{ItemName="Legs Fitter",Image="/pant.jpg",Price=4000,Discount=50}
+        };
+    }
+}
