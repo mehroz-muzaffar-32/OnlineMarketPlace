030e885a2dc1b21bffb1c18e253bde08ed88714a
mehroz-muzaffar-SEF18 <79619614+mehroz-muzaffar-SEF18@users.noreply.github.com>
2021-06-22 12:49:55 +0500

    Backend with Seller Items List v1.1

--- OnlineMarketPlaceTorico/OnlineMarketPlaceTorico/OnlineMarketPlaceTorico/Shared/ItemsRepository.cs
+++ OnlineMarketPlaceTorico/OnlineMarketPlaceTorico/OnlineMarketPlaceTorico/Shared/ItemsRepository.cs
@@ -9,9 +9,9 @@ namespace OnlineMarketPlaceTorico.Shared
     public class ItemsRepository
     {
         static public string View { get; set; } = "List";
-        static public int CurrentSellerId = 0;
-        static public List<Product> ItemsList { get; set; } = new List<Product>();
-        static public List<Category> Categories { get; set; } = new List<Category>();
+        static public int CurrentSellerId = -1;
+        static public List<Product> ItemsList { get; set; } = null;
+        static public List<Category> Categories { get; set; } = null;
         static public List<string> Pictures { get; set; } = new List<string> { "pant.jpg", "shirt.jpg", "shoes.jpg","shoes2.jpg","shoes3.jpg","shoes4.jpg","watch.jpg","watch2.jpg","watch3.jpg","watch4.jpg"};
     }
 }
