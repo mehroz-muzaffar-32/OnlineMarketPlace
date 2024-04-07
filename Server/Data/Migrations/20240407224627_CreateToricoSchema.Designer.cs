606f57a7ad5faafd55b3601d2a37ee27fd562ba0
Mehroz Muzaffar <mehroze32@gmail.com>
2024-04-08 03:52:30 +0500

    chore: created and applied migration for torico schema

--- Server/Data/Migrations/20240407224627_CreateToricoSchema.Designer.cs
+++ Server/Data/Migrations/20240407224627_CreateToricoSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineMarketPlaceTorico.Server.Data;
 
@@ -11,9 +12,10 @@ using OnlineMarketPlaceTorico.Server.Data;
 namespace OnlineMarketPlaceTorico.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240407224627_CreateToricoSchema")]
+    partial class CreateToricoSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
